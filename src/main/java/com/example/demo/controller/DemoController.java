2ba2667f661ec1089c86b08aabb43f0f6cfe7c91
sahil-dbs <sahil.gupta.dbs@gmail.com>
2023-01-14 04:24:55 +0530

    changes from local..

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,3 +1,16 @@
 package com.example.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@Slf4j
+class DemoController{
+
+    @GetMapping(name = "/test")
+    public String test123(){
+    log.info("/test api invoked.. ");
+    return "smthinhg..";
+    }
+}
